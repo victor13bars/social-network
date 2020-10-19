76fe6925761e0614a5f5292bf094941ea0e787ec
victor13bars <victor_bars_bars95@mail.ru>
2020-10-19 22:03:18 +0300

    Updates

---
+++ precache-manifest.04cfd9e5b7d269c8a7f0e70f0f0a4528.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5af0ee7436b363f5b7babd0a756c1758",
+    "url": "/social-network/index.html"
+  },
+  {
+    "revision": "aa61a391daf2050d2f2c",
+    "url": "/social-network/static/css/main.12ebbdc9.chunk.css"
+  },
+  {
+    "revision": "e844e45521a25a7c3f1f",
+    "url": "/social-network/static/js/2.89632c57.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/social-network/static/js/2.89632c57.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa61a391daf2050d2f2c",
+    "url": "/social-network/static/js/main.8883c16b.chunk.js"
+  },
+  {
+    "revision": "bea12d42c5cb6573022c",
+    "url": "/social-network/static/js/runtime-main.7af2152f.js"
+  }
+]);

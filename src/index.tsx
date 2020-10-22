f3bcc06660e6e1e4dc2b9b7804afaef99a7f13b9
victor13bars <victor_bars_bars95@mail.ru>
2020-10-22 19:14:35 +0300

    Add store. Move functions and state to store.

--- src/index.tsx
+++ src/index.tsx
@@ -3,18 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import state, {addPost, StateType, subscribe, updateNewPostText} from "./redux/state";
+import store from "./redux/state";
 
 let rerenderEntireTree = () => {
     ReactDOM.render(
         <React.StrictMode>
-            <App state={state} addPost={addPost} updateNewPostText={updateNewPostText}/>
+            <App state={store.getState()} addPost={store.addPost.bind(store)}
+                 updateNewPostText={store.updateNewPostText.bind(store)}/>
         </React.StrictMode>,
         document.getElementById('root')
     );
 };
 rerenderEntireTree();
-subscribe(rerenderEntireTree);
+store.subscribe(rerenderEntireTree);
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
--- src/redux/state.ts
+++ src/redux/state.ts
@@ -1,85 +1,92 @@
-let rerenderEntireTree = () => {
-    console.log("State change");
-}
-
-export type SidebarType = {}
-
-export type DialogType = {
-    id: number
-    name: string
-}
-
-export type MessageType = {
-    id: number
-    message: string
-}
-
 export type PostType = {
     id: number
     message: string
     likeCount: number
 }
-
-
 export type ProfilePageType = {
     posts: Array<PostType>
     messageForNewPost: string
 }
 
+export type DialogType = {
+    id: number
+    name: string
+}
+export type MessageType = {
+    id: number
+    message: string
+}
 export type MessagesPagesType = {
     dialogs: Array<DialogType>
     messages: Array<MessageType>
 }
+
 export type StateType = {
     profilePage: ProfilePageType
     dialogsPage: MessagesPagesType
     sidebar: SidebarType
 }
 
-export let state: StateType = {
-    profilePage: {
-        posts: [
-            {id: 1, message: "Hello", likeCount: 12},
-            {id: 2, message: "How are you?", likeCount: 25},
-        ],
-        messageForNewPost: ""
+export type SidebarType = {}
+
+export type StoreType = {
+    _state: StateType
+    updateNewPostText: (newText: string) => void
+    addPost: () => void
+    _callSubscriber: () => void
+    subscribe: (observer: () => void) => void
+    getState: () => StateType
+}
+
+let store: StoreType = {
+    _state: {
+        profilePage: {
+            posts: [
+                {id: 1, message: "Hello", likeCount: 12},
+                {id: 2, message: "How are you?", likeCount: 25},
+            ],
+            messageForNewPost: ""
+        },
+        dialogsPage: {
+            dialogs: [
+                {id: 1, name: "Victor"},
+                {id: 2, name: "Valera"},
+                {id: 3, name: "Veronika"},
+                {id: 4, name: "Alex"},
+                {id: 5, name: "Dima"},
+                {id: 6, name: "Toxa"}
+            ],
+            messages: [
+                {id: 1, message: "Hello"},
+                {id: 2, message: "How are you?"},
+                {id: 3, message: "kikii"},
+                {id: 4, message: "Alex"},
+                {id: 5, message: "Dima"},
+                {id: 6, message: "Toxa"}
+            ]
+        },
+        sidebar: {}
+
     },
-    dialogsPage: {
-        dialogs: [
-            {id: 1, name: "Victor"},
-            {id: 2, name: "Valera"},
-            {id: 3, name: "Veronika"},
-            {id: 4, name: "Alex"},
-            {id: 5, name: "Dima"},
-            {id: 6, name: "Toxa"}
-        ],
-        messages: [
-            {id: 1, message: "Hello"},
-            {id: 2, message: "How are you?"},
-            {id: 3, message: "kikii"},
-            {id: 4, message: "Alex"},
-            {id: 5, message: "Dima"},
-            {id: 6, message: "Toxa"}
-        ]
+    getState() {
+        return this._state;
     },
-    sidebar: {}
-
+    _callSubscriber() {
+        console.log("State change");
+    },
+    addPost() {
+        const newPost: PostType = {id: 5, message: this._state.profilePage.messageForNewPost, likeCount: 0};
+        this._state.profilePage.posts.push(newPost);
+        this._state.profilePage.messageForNewPost = "";
+        this._callSubscriber();
+    },
+    updateNewPostText(newText: string) {
+        this._state.profilePage.messageForNewPost = newText;
+        this._callSubscriber();
+    },
+    subscribe(observer) {
+        this._callSubscriber = observer;
+    }
 }
 
-export const addPost = () => {
-    const newPost: PostType = {id: 5, message: state.profilePage.messageForNewPost, likeCount: 0};
-    state.profilePage.posts.push(newPost);
-    state.profilePage.messageForNewPost = "";
-    rerenderEntireTree();
-}
-
-export const updateNewPostText = (newText: string) => {
-    state.profilePage.messageForNewPost = newText;
-    rerenderEntireTree();
-}
-
-export const subscribe = (observer: () => void) => {
-    rerenderEntireTree = observer;
-}
-
-export default state;
+export default store;

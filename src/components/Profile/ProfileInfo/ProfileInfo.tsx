3b23ba129e09b770eb276b79b03adc8a528fdd4b
victor13bars <victor_bars_bars95@mail.ru>
2021-02-12 14:35:15 +0300

    71. React JS - setState, local state.

--- src/components/Profile/ProfileInfo/ProfileInfo.tsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import classes from './ProfileInfo.module.css';
 import Preloader from "../../common/Preloader/Preloader";
 import {ProfilePropsType} from "../Profile";
-
+import ProfileStatus from './ProfileStatus';
 
 const ProfileInfo = (props:ProfilePropsType) => {
     if(!props.profile){
@@ -10,16 +10,16 @@ const ProfileInfo = (props:ProfilePropsType) => {
     }
     return (
         <div>
-            <div>
-                <img className={classes.img}
-                     src="https://avatars.mds.yandex.net/get-pdb/2797954/0e70c09d-6fa7-40ac-ba85-c36b467e8de5/s1200?webp=false"
-                     alt=""/>
-            </div>
+            {/*<div>*/}
+            {/*    <img className={classes.img}*/}
+            {/*         src="https://avatars.mds.yandex.net/get-pdb/2797954/0e70c09d-6fa7-40ac-ba85-c36b467e8de5/s1200?webp=false"*/}
+            {/*         alt=""/>*/}
+            {/*</div>*/}
             <div className={classes.descriptionBlock}>
                 <img src={props.profile.photos.large} alt=""/>
                 <div>{props.profile.aboutMe}</div>
                 <div>{props.profile.lookingForAJobDescription}</div>
-                Ava+description
+                <ProfileStatus status={'Никто кроме тебя'}/>
             </div>
 
         </div>
